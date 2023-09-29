4a6b08729d36ebb03cd1ade232cb7ab69990164b
humy06001 <humy06001@E-PW0234KS>
2023-09-29 10:18:40 +0200

    Add project files.

---
+++ Kap 3 Uppgift 3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Uppgift3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? \nSvara ett \"j\" för ja, eller ett \"n\" för nej.");
+            string gymnasie = Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du? Svara med ett heltal.");
+            int age = int.Parse(Console.ReadLine());
+
+            if (gymnasie == "j" && age < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
